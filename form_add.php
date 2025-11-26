d78f3c13bd4806155a48fe6a3e29cc684912734d
Arbani <159100563+Girinko@users.noreply.github.com>
2025-11-26 15:51:30 +0700

    php_template

---
+++ form_add.php
@@ -0,0 +1,48 @@
+<?php
+include 'inc/header.php';
+?>
+<div class="container-fluid">
+
+                    <!-- Page Heading -->
+                    <h1 class="h3 mb-4 text-gray-800">Blank Page</h1>
+                    <div class="card">
+                        <form>
+                            <div class="card-body">
+                                <div class="form-group">
+                                    <label for="exampleInputUsername">Username</label>
+                                    <input type="text" class="form-control" id="exampleInputUsername" aria-describedby="usernameHelp">
+                                </div>
+                                <div class="form-group">
+                                    <label for="exampleInputPassword">Password</label>
+                                    <input type="password" class="form-control" id="exampleInputPassword" aria-describedby="passwordHelp">
+                                </div>
+                                <div class="form-group">
+                                    <label for="exampleInputEmail">Email</label>
+                                    <input type="email" class="form-control" id="exampleInputEmail" aria-describedby="emailHelp">
+                                </div>
+                                <div class="form-group">
+                                    <label for="exampleInputFirstName">First Name</label>
+                                    <input type="text" class="form-control" id="exampleInputFirstName" aria-describedby="firstnameHelp">
+                                </div>
+                                <div class="form-group">
+                                    <label for="exampleInputLastName">Last Name</label>
+                                    <input type="text" class="form-control" id="exampleInputLastName" aria-describedby="lastnameHelp">
+                                </div>
+                                <div class="form-group">
+                                    <label for="exampleInputCompany">Company</label>
+                                    <input type="text" class="form-control" id="exampleInputCompany" aria-describedby="companyHelp">
+                                </div>
+                                <div class="form-group">
+                                    <label for="examplePhone">Phone</label>
+                                    <input type="number" class="form-control" id="exampleInputPhone" aria-describedby="phoneHelp">
+                                </div>
+                            </div>
+                            <div class="card-footer">
+                                <button type="submit" class="btn btn-primary">Submit</button>
+                            </div>
+                        </form>
+                    </div>
+                </div>
+<?php
+include 'inc/footer.php';
+?>
